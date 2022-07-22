47a21f12aa21be6a4fd249534c2c0595069f98c6
Terezzinha <terezinhanr@gmail.com>
2022-07-22 16:45:48 -0300

    resolvendo conflitos

--- files.py
+++ files.py
@@ -18,7 +18,6 @@ with open("../../arquivos_auxiliares/estudo_path_py.txt") as file:
     contents = file.read()
     print(contents)
 
-<<<<<<< HEAD
 # Sobe até pasta-mãe comum. main está em day24_Mail_Merge_Project
 with open("./Input/Letters/starting_letter.txt", "r") as fl_latter:
     txt_latter = fl_latter.read()
@@ -33,15 +32,3 @@ with open("../day24_Mail_Merge_Project/Input/Letters/starting_letter.txt", "r") 
 # Ref: https://www.w3schools.com/python/ref_file_readlines.asp
 f = open("demofile.txt", "r")
 print(f.readlines())
-=======
-#CSV
-import csv
-
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != 'temp':
-            temperature.append(int(row[1]))
-    print(temperature)
->>>>>>> 1f2319503a7e9e24e7712dd11344757096554ff6
