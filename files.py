335433890302f6db9b29971288971673ce037616
Terezzinha <terezinhanr@gmail.com>
2022-07-22 16:39:50 -0300

    atulização diversa

--- files.py
+++ files.py
@@ -8,11 +8,27 @@ with open("meu_arquivo.txt") as file:
 with open("meu_arquivo.txt", mode="a") as file:
     file.write("\nlinha 2")
 
-# Abrindo arquivo e sobrescrevendo
+# Criando/abrindo arquivo e sobrescrevendo
 with open("criar_arquivo.txt", mode="w") as file:
-  file.write("\nlinha q")
+    file.write("\nlinha q")
 
-#Paths
+# Paths
+# !!Colocar o nome da pasta para qual "subiu"
 with open("../../arquivos_auxiliares/estudo_path_py.txt") as file:
     contents = file.read()
     print(contents)
+
+# Sobe até pasta-mãe comum. main está em day24_Mail_Merge_Project
+with open("./Input/Letters/starting_letter.txt", "r") as fl_latter:
+    txt_latter = fl_latter.read()
+    print(txt_latter)
+# OU
+with open("../day24_Mail_Merge_Project/Input/Letters/starting_letter.txt", "r") as fl_latter:
+    txt_latter = fl_latter.read()
+    print(txt_latter)
+
+
+# Return all lines in the file, as a list where each line is an item in the list object:
+# Ref: https://www.w3schools.com/python/ref_file_readlines.asp
+f = open("demofile.txt", "r")
+print(f.readlines())
---
+++ strings.py
@@ -0,0 +1,4 @@
+# Retirando carcateres especiais
+name = "Zuko\n"
+just_name = name.strip('\n')
+nm_file = "ready_to_send_" + just_name + ".txt"
